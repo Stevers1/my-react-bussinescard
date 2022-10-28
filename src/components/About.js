889dd4691314da001d1699dd13157c940bc6de87
Steve <uberkootra@gmail.com>
2022-10-28 18:33:09 -0500

    Fix typos

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ function About() {
     <div className="about-container">
       <div className="about">
         <h2>About</h2>
-        <p>Hello! Im Esteban, but you can call me Steve. <br></br> I am a frontend developer intersted in making things easier and in automating daily trasks while learning new tecnologies too. Feel free to contact and ask me anything!
+        <p>Hello! Im Esteban, but you can call me Steve. <br></br> I am a frontend developer interested in making things easier and in automating daily trasks while learning new tecnologies too. Feel free to contact and ask me anything!
         </p>
       </div>
       <div className="interests">
         <h2>Skills</h2>
-        <p>CSS, HTML5, Node, JavaScript, React, Ruby, Git, Bash, Music producer,</p>
+        <p>CSS, HTML5, Node, JavaScript, React, Ruby, Git, Bash, Music producer</p>
       </div>
     </div>
   );
