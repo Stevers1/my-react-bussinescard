711e25ca7c10e5521d7691f42a08707e340a6216
Steve <uberkootra@gmail.com>
2022-10-28 18:01:31 -0500

    Add selfie

--- src/components/Btns.js
+++ src/components/Btns.js
@@ -4,7 +4,7 @@ import letter from '../images/iconletter.png'
 function Btns() {
   return (
     <div className="btns-container">
-      <button className="btn-email"><img src={letter}/>Email</button>
+      <button className="btn-email"><img alt='letter icon' src={letter}/>Email</button>
       <button className="btn-linked">LinkedIn</button>
     </div>
   );
